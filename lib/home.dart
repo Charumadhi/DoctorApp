dab0af27e25eceadc425f897185033790f5c320f
Charumadhi <charumadhithourai@gmail.com>
2024-10-15 20:56:56 +0530

    Changed

--- lib/home.dart
+++ lib/home.dart
@@ -8,13 +8,13 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final arguments = ModalRoute.of(context)?.settings.arguments;
     // If arguments are not null, cast to Map<String, String>
-    final Map<String, dynamic> doctorInfo = arguments as Map<String, dynamic>;
+    final Map<String, dynamic> doctor = arguments as Map<String, dynamic>;
 
     // Get the doctor name and ID from the arguments
-    final String doctorName = doctorInfo['doctorName'] ?? 'Unknown';
-    final String doctorId = doctorInfo['doctorId'] ?? 'Unknown';
-    final String area = doctorInfo['area'] ?? 'Unknown';
-    final String district = doctorInfo['district'] ?? 'Unknown';
+    final String doctorName = doctor['doctorName'] ?? 'Unknown';
+    final String doctorId = doctor['doctorId'] ?? 'Unknown';
+    final String area = doctor['area'] ?? 'Unknown';
+    final String district = doctor['district'] ?? 'Unknown';
 
     return Scaffold(
       extendBodyBehindAppBar: true,
@@ -134,7 +134,7 @@ class HomePage extends StatelessWidget {
               context,
               MaterialPageRoute(
                 builder: (context) => StatisticsPage(),
-                settings: RouteSettings(arguments: doctorInfo), // Pass doctorInfo to StatisticsPage
+                settings: RouteSettings(arguments: doctor), // Pass doctorInfo to StatisticsPage
               ),
             );
           } else if (index == 2) {
@@ -142,7 +142,7 @@ class HomePage extends StatelessWidget {
               context,
               MaterialPageRoute(
                 builder: (context) => ProfilePage(),
-                settings: RouteSettings(arguments: doctorInfo), // Pass doctorInfo to ProfilePage
+                settings: RouteSettings(arguments: doctor), // Pass doctorInfo to ProfilePage
               ),
             );
           }
